ce46bc4667444e1b8f383f896a369b3e1cb1dadf
gooi-me <gooicapetown@gmail.com>
2023-11-02 13:55:24 +0200

    added groei and why

--- scripts.js
+++ scripts.js
@@ -5,6 +5,11 @@ function signMeUpPlease(){
     window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScrQd_U_JMxDQTwtgiTyMS1AABlRdpiPjihJawnlG2Vk-7zlg/viewform"
 }
 
+function countMeInPlease(){
+  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfQjYjvhPLIV2JBNMphRg5jrLDVR4SECPDj4abbBs5sNKK5ng/viewform?usp=sf_link"
+}
+
+
 
 
 /* Listens for when the knoppie is drukked and tells google tag manager that it's been drukked. */
